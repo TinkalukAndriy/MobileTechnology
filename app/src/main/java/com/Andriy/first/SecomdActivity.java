8e814605fac8edc55adddb3a423b8c80aabff93e
TinkalukAndriy <andrew.tynkalyuk@kpk-lp.com.ua>
2023-04-14 06:56:10 +0300

    secondLab

--- app/src/main/java/com/Andriy/first/SecomdActivity.java
+++ app/src/main/java/com/Andriy/first/SecomdActivity.java
@@ -1,12 +1,18 @@
 package com.Andriy.first;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SecomdActivity extends AppCompatActivity {
-
+ private EditText usernameEditText;
+ private EditText passwordEditText;
+ private SharedPreferences pref;
+ private final String save_key = "save_key";
+ private final String save_key1 = "save_key";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,4 +22,16 @@ public class SecomdActivity extends AppCompatActivity {
     public void onClick (View v) {
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);}
+
+    public void OnclickSave (View v) {
+    SharedPreferences.Editor edit = pref.edit();
+    edit.putString(save_key,usernameEditText.getText().toString());
+    edit.putString(save_key1,passwordEditText.getText().toString());
+    edit.apply();
+    }
+    public void init (View V) {
+        pref = getSharedPreferences("text", MODE_PRIVATE);
+        usernameEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextNumberPassword);
+    }
 }
--- app/src/main/java/com/Andriy/first/ThirdActivity.java
+++ app/src/main/java/com/Andriy/first/ThirdActivity.java
@@ -1,12 +1,18 @@
 package com.Andriy.first;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    private EditText usernameEditText1;
+    private EditText passwordEditText1;
+    private SharedPreferences pref1;
+    private final String save_key2 = "save_key";
+    private final String save_key3 = "save_key";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,4 +22,16 @@ public class ThirdActivity extends AppCompatActivity {
     public void onClick (View v) {
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);}
+
+    public void OnclickSave1 (View v) {
+        SharedPreferences.Editor edit = pref1.edit();
+        edit.putString(save_key2,usernameEditText1.getText().toString());
+        edit.putString(save_key3,passwordEditText1.getText().toString());
+        edit.apply();
+    }
+    public void init (View V) {
+        pref1 = getSharedPreferences("text", MODE_PRIVATE);
+        usernameEditText1 = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText1 = findViewById(R.id.editTextNumberPassword);
+    }
 }
